46c48026f78365415e06aeb1664e1837460fd569
ho94949 <sakura@kyouko.moe>
2023-07-11 13:21:16 +0900

    Modified typo

--- gym_pinpad/envs/pinpad.py
+++ gym_pinpad/envs/pinpad.py
@@ -61,7 +61,7 @@ class PinPadEnv(gym.Env):
     @property
     def obs_space(self):
         spaces = {
-            "observation": self._env.observation_space,
+            "observation": self.observation_space,
             "reward": gym.spaces.Box(-np.inf, np.inf, (), dtype=np.float32),
             "is_first": gym.spaces.Box(0, 1, (), dtype=bool),
             "is_last": gym.spaces.Box(0, 1, (), dtype=bool),
