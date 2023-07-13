4f63b6b9cabe4e74c10287e6615533182ee28f3b
ho94949 <sakura@kyouko.moe>
2023-07-13 11:24:56 +0900

    Modified pinpad version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='gym_pinpad',
-    version='2023.7.11',
+    version='2023.7.13',
     keywords='environment, agent, rl, openaigym, openai-gym, gym, 2d',
     packages=['gym_pinpad', 'gym_pinpad.envs'],
     install_requires=[
         'gym>=0.9.0',
-        'numpy>=1.20.0',
+        'numpy>=1.10.0',
     ],
     extras_require={'gui': ['pygame', 'Pillow']},
     entry_points={'console_scripts': ['gym_pinpad=gym_pinpad.run_gui:main']},
